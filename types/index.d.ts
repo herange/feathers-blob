15dab17be354dab28b9e176827ccf3bac4d107fa
Kyrylo Stas <vesground@gmail.com>
2023-02-01 11:08:43 +0200

    Fixed types to extend the Service in Typescript

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,33 +1,24 @@
 // TypeScript Version: 3.0
-
 import { AbstractBlobStore } from 'abstract-blob-store';
 import { Params, Id } from '@feathersjs/feathers';
-
 export = feathersBlob;
-
 declare function feathersBlob(config: feathersBlob.InitOptions): feathersBlob.Service;
-
 interface UnknownObject {
   [propName: string]: any;
 }
-
 interface RemoveResultWithCustomIdParam {
   [idParam: string]: string;
 }
-
 interface RemoveResultWithId {
   id: string;
 }
-
 interface CreateUriBody {
   uri: string;
 }
-
 interface CreateBufferBody {
   buffer: Buffer;
   contentType: string;
 }
-
 declare namespace feathersBlob {
   interface InitOptions {
     returnUri?: boolean;
@@ -35,31 +26,26 @@ declare namespace feathersBlob {
     Model: AbstractBlobStore;
     id?: string;
   }
-
   interface BlobParams extends Params {
     s3?: UnknownObject;
   }
-
   type CreateBody = CreateUriBody | CreateBufferBody;
-
   type RemoveResult = RemoveResultWithId | RemoveResultWithCustomIdParam;
-
   interface BlobResult {
     id?: string;
     buffer?: Buffer;
     uri?: string;
     size: number;
     [idParam: string]: any;
-    contentType: string;
   }
 
-  interface Service {
+  class Service {
+    constructor(options: InitOptions)
+
     extend(obj: UnknownObject): Service;
 
     get(id: Id, params?: BlobParams): Promise<BlobResult>;
-
     create(body: CreateBody, params?: BlobParams): Promise<BlobResult>;
-
     remove(id: Id, params?: Params): Promise<RemoveResult | RemoveResultWithCustomIdParam>;
   }
 }
